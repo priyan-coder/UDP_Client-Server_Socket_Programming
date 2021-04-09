2b2cd0a53b6b75ae6a2101b8217b711efdaa31b4
priyan_rajamohan <rajamohanpriyan@gmail.com>
2021-04-09 16:15:53 +0800

    kind of coded out the logic already, now need to verify that it works

--- udp_client.py
+++ udp_client.py
@@ -1,17 +1,13 @@
 import socket
-# https://docs.python.org/3/library/socket.html
 from sys import argv
-# https://www.geeksforgeeks.org/python-os-stat-method/
 from os import stat
-# https://stackoverflow.com/questions/14215715/reading-a-binary-file-into-a-struct
 from struct import *
-# https://docs.python.org/3/library/struct.html#struct.pack
 from time import time_ns
-# https://www.geeksforgeeks.org/python-time-time_ns-method/
+from math import ceil
+from hashlib import sha256
 
-# specify the PORT_NUM, MAXIMUM_SIZE_OF_FILE_TO_TRANSFER, HEADER_SIZE, ACK_SIZE
-PORT, MAXSIZE, HEADER, ACK_SIZE = 4950, 30000, 4, 1
-# run the program in cmd like this: tcp_client2.py localhost myfile.txt
+PORT, MAX_FILE_SIZE, ACK_SIZE = 4950, 60000, 4
+# run in cmd like this: tcp_client3.py localhost myfile.txt 500 (eg. if packet size is 500)
 try:
     HOST = argv[1]
 except IndexError:
@@ -22,45 +18,42 @@ try:
 except IndexError:
     FILE = input("Enter filename: ")
 
-# get the size of the file to be transferred in bytes 
 size = stat(FILE).st_size
-if size > MAXSIZE:
-    raise ValueError(f"File size exceeds limit of {MAXSIZE} B")
+if size > MAX_FILE_SIZE:
+    raise ValueError(f"File size exceeds limit of {MAX_FILE_SIZE} B")
 
-# Opens the file as read-only in binary format and starts reading from the beginning of the file. 
-with open(FILE, "rb") as f:
-    # msg is a binary array 
-    msg = f.read()
-
-# Packet in the format : Network order i.e. Big Endian ==> Unsigned int, string of char[size]
-# sending the size of the file and its binary byte stream
-packet = pack(f"!I{size}s", size, msg)
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:        
-    s.connect((HOST, PORT))
-    # start timer in nano sec 
-    start = time_ns()  
-    # sending all packet until data sent, returns NONE, when all data sent or error
-    s.sendall(packet)
-    # unpack in the format : Network Order ==> unsigned char of 1 byte 
-    # ack accesses the unpacked acknowledgement from server which is the first element of the tuple 
-    ack = unpack(f"!{ACK_SIZE}B", s.recv(ACK_SIZE))[0]
-
-# stop timer when received ack 
-rtt = time_ns() - start
-size += HEADER
-print(f"Received ACK{ack}\nTransferred {size} B in {rtt} ns")
-
-# calculation of data rate 
 try:
-    data_rate = (size/1024) / (rtt/(10**9))
-    print(f"Data rate: {data_rate} KB/s")
-except ZeroDivisionError:
-    pass
+    DATA_UNIT_SIZE = int(argv[3])
+except IndexError:
+    DATA_UNIT_SIZE = int(input("Enter packet size (B): "))
 
-# checkin congruecny between sent and received 
-with open("myfile.txt", newline="") as f1, open("received.txt", newline="") as f2:
-    txt1, txt2 = f1.read(), f2.read()
-if txt2 == txt1:
-    print("Received message is identical to original")
-else:
-    print("Received message is different from original")
+
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s, open(FILE, "rb") as f:        
+    packet = pack("!II", DATA_UNIT_SIZE, size)
+    s.sendto(packet, (HOST,PORT))
+    full_msg = bytearray()
+    start = time_ns()
+    sent = 0
+    j = 1
+    while(sent < size):
+        if(j > 3):
+            j = 1
+        msg = f.read(j*DATA_UNIT_SIZE)
+        full_msg.extend(msg)
+        msg_size = len(msg)
+        packet = pack(f"{msg_size}s", msg)
+        s.sendto(packet, (HOST,PORT))
+        if(ack = unpack(f"!I", s.recvfrom(ACK_SIZE))[0]):
+            print(f"Received ACK{ack}\nTransferred {msg_size} Bytes\n")
+            continue
+        j+=1
+        sent+=msg_size
+
+transfer_time = time_ns() - start
+print(f"Transferred {size} Bytes in {transfer_time} ns \n")
+# try:
+#     data_rate = (size/1024) / (rtt/(10**9))
+#     print(f"Data rate: {data_rate} KB/s")
+# except ZeroDivisionError:
+#     pass
+# print(f"SHA256 checksum of sent data: {sha256(full_msg).hexdigest()}")
--- udp_server.py
+++ udp_server.py
@@ -1,35 +1,21 @@
-# https://docs.python.org/3/library/socket.html
 import socket
-# https://docs.python.org/3/library/struct.html
 from struct import *
+from hashlib import sha256
 
+HOST, PORT, HEADER = "", 4950, 8
 
-HOST, PORT, HEADER, ACK_SIZE, ack_num = "", 4950, 4, 1, 1
-# The HEADER is 4 bytes in which the size of the msg to be 
-# received from client upon unpacking is specified
-# This is because size of integer is 4 bytes
-
-# creation of socket s and binding it to the host and port
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
     s.bind((HOST, PORT))
-    s.listen()
     while True:
-        conn, addr = s.accept()
-        with conn:
-            print(f"Connected by {addr}")
-            # unpack to convert byte stream received in network order i.e. big endian ==> unsigned integers
-            # size contains the size of msg to be received 
-            size = unpack("!I", conn.recv(HEADER))[0]
-            # msg is a bytes stream or bytes object 
-            msg = conn.recv(size)
-            # acks is a bytes oject that contains acknowledgement num formatted into a 1 byte unsigned char
-            ack = pack(f"!{ACK_SIZE}B", ack_num)
-            conn.sendall(ack)
-        with open("received.txt", "wb") as f:
-            f.write(msg)
-        print(f"Sent ACK{ack_num}")
-        ack_num += 1
-        # print(msg)
-
-
-# https://stackoverflow.com/questions/27893804/udp-client-server-socket-in-python
+            msg = bytearray()
+            pack_size, total_size = unpack("!II", s.recvfrom(HEADER))
+            j = 1
+            while len(msg) < total_size:
+                if(j > 3):
+                    j = 1
+                msg.extend(s.recvfrom(j*pack_size))
+                ack = pack(f"!I",j*pack_size)
+                s.sendto(ack, (HOST, PORT))
+                j+=1
+        print(f"Received {len(msg) + HEADER} B packets")
+        # print(f"SHA256 checksum of received data: {sha256(msg).hexdigest()}")
