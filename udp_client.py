be566c0fed7ed021be663c24732262caaf1c9fa2
priyan_rajamohan <rajamohanpriyan@gmail.com>
2021-04-09 23:52:50 +0800

    managed to get the code to work. Need to fix up some stuff and work on checksum later

--- udp_client.py
+++ udp_client.py
@@ -1,12 +1,13 @@
 import socket
 from sys import argv
 from os import stat
-from struct import *
-from time import time_ns
 from math import ceil
-from hashlib import sha256
+from struct import *
 
-PORT, MAX_FILE_SIZE, ACK_SIZE = 4950, 60000, 4
+PORT = 4950
+MAX_FILE_SIZE = 60000
+ACK_BUFFER = 4
+
 # run in cmd like this: udp_client.py localhost myfile.txt 500 (eg. if packet size is 500)
 try:
     HOST = argv[1]
@@ -19,8 +20,9 @@ except IndexError:
     FILE = input("Enter filename: ")
 
 size = stat(FILE).st_size
+print(f"Size of file asked for transfer : {size}\n")
 if size > MAX_FILE_SIZE:
-    raise ValueError(f"File size exceeds limit of {MAX_FILE_SIZE} B")
+    raise ValueError(f"File size exceeds limit of {MAX_FILE_SIZE} B\n")
 
 try:
     DATA_UNIT_SIZE = int(argv[3])
@@ -28,38 +30,96 @@ except IndexError:
     DATA_UNIT_SIZE = int(input("Enter packet size (B): "))
 
 num_packs = ceil(size/DATA_UNIT_SIZE)
-printf(f"Expected total data units for transfer : {num_packs}\n")
+print(f"Expected total number of data units for transfer : {num_packs}\n")
 
 
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s, open(FILE, "rb") as f:        
-    packet = pack("!II", DATA_UNIT_SIZE, size)
-    s.sendto(packet, (HOST,PORT))
-    full_msg = bytearray()
-    start = time_ns()
-    count = 0
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s, open(FILE, "rb") as f:
+    # s.sendto(bytes(str(size)+"\0", encoding="utf8"), (HOST, PORT))
+    # s.sendto(bytes(str(DATA_UNIT_SIZE)+"\0",  encoding="utf8"), (HOST, PORT))
+    pkt = pack("!II", size, DATA_UNIT_SIZE)
+    s.sendto(pkt, (HOST, PORT))
     sent = 0
-    j = 1
-    while(sent < size):
-        if(j > 3):
-            j = 1
-        msg = f.read(j*DATA_UNIT_SIZE)
-        full_msg.extend(msg)
-        msg_size = len(msg)
-        packet = pack(f"{msg_size}s", msg)
-        s.sendto(packet, (HOST,PORT))
-        if(ack = unpack(f"!I", s.recvfrom(ACK_SIZE))[0]):
-            count += j
-            print(f"Received ACK{ack}\nTransferred {msg_size} Bytes\n")
-            continue
-        j+=1
-        sent+=msg_size
+    batch = 1
+    while sent < size:
+        if(batch > 3):
+            batch = 1
+        msg = f.read(batch*DATA_UNIT_SIZE)
+        s.sendto(msg, (HOST, PORT))
+        ack, srv_addr = s.recvfrom(ACK_BUFFER)
+        ack = int(ack.decode("utf8"))
+        print(
+            f"Currenlty received total file size is {ack} bytes at server with addr : {srv_addr}\n")
+        sent += batch * DATA_UNIT_SIZE
+        print(f"Sent by Client : {sent} Bytes\n")
+        batch += 1
 
-transfer_time = time_ns() - start
-print(f"Transferred {count} data units \n")
-print(f"Transferred {size} Bytes in {transfer_time} ns \n")
+
+# import socket
+# from sys import argv
+# from os import stat
+# from struct import *
+# # from time import time_ns
+# from math import ceil
+# from hashlib import sha256
+# import sys
+
+# PORT, MAX_FILE_SIZE, ACK_SIZE = 4950, 60000, 4
+# # run in cmd like this: udp_client.py localhost myfile.txt 500 (eg. if packet size is 500)
 # try:
-#     data_rate = (size/1024) / (rtt/(10**9))
-#     print(f"Data rate: {data_rate} KB/s")
-# except ZeroDivisionError:
-#     pass
-# print(f"SHA256 checksum of sent data: {sha256(full_msg).hexdigest()}")
+#     HOST = argv[1]
+# except IndexError:
+#     HOST = input("Enter host: ")
+
+# try:
+#     FILE = argv[2]
+# except IndexError:
+#     FILE = input("Enter filename: ")
+
+# size = stat(FILE).st_size
+# if size > MAX_FILE_SIZE:
+#     raise ValueError(f"File size exceeds limit of {MAX_FILE_SIZE} B")
+
+# try:
+#     DATA_UNIT_SIZE = int(argv[3])
+# except IndexError:
+#     DATA_UNIT_SIZE = int(input("Enter packet size (B): "))
+
+# num_packs = ceil(size/DATA_UNIT_SIZE)
+# print(f"Expected total data units for transfer : {num_packs}\n")
+
+
+# # with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s, open(FILE, "rb") as f:
+# #     fmt = calcsize("!II")
+# #     print(f"size of format sent : {fmt}")
+# #     pkt = pack("!II", DATA_UNIT_SIZE, size)
+# #     print(f"Sending {sys.getsizeof(pkt)} bytes packet\n")
+# #     s.sendto(pkt, (HOST,PORT))
+# #     full_msg = bytearray()
+# #     # start = time_ns()
+# #     count = 0
+# #     sent = 0
+# #     j = 1
+# #     while(sent < size):
+# #         if(j > 3):
+# #             j = 1
+# #         msg = f.read(j*DATA_UNIT_SIZE)
+# #         full_msg.extend(msg)
+# #         msg_size = len(msg)
+# #         packet = pack(f"{msg_size}s", msg)
+# #         s.sendto(packet, (HOST,PORT))
+# #         count += j
+# #         if(count == unpack(f"!I", s.recvfrom(ACK_SIZE))[0]):
+# #             print(f"Received ACK{count}\nTransferred {msg_size} Bytes\n")
+# #             continue
+# #         j+=1
+# #         sent+=msg_size
+
+# # # transfer_time = time_ns() - start
+# # print(f"Transferred {count} data units \n")
+# # # print(f"Transferred {size} Bytes in {transfer_time} ns \n")
+# # # try:
+# # #     data_rate = (size/1024) / (rtt/(10**9))
+# # #     print(f"Data rate: {data_rate} KB/s")
+# # # except ZeroDivisionError:
+# # #     pass
+# # # print(f"SHA256 checksum of sent data: {sha256(full_msg).hexdigest()}")
--- udp_server.py
+++ udp_server.py
@@ -1,23 +1,62 @@
+# import socket
+# from struct import *
+# from hashlib import sha256
+# import sys
+
+# HOST, PORT, HEADER = "", 4950, 8
+
+# with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+#     s.bind((HOST, PORT))
+#     while True:
+#             msg = bytearray()
+#             count = 0
+#             received_stream, addr = s.recvfrom(HEADER)
+#             print(f"Connected by : {addr}")
+#             print(f"size of object received : {sys.getsizeof(received_stream)}")
+#             DATA_UNIT_SIZE, TOTAL_FILE_SIZE = unpack("!II", received_stream)
+#             j = 1
+#             while len(msg) < TOTAL_FILE_SIZE:
+#                 if(j > 3):
+#                     j = 1
+#                 msg.extend(s.recvfrom(j*DATA_UNIT_SIZE)[0])
+#                 count += j
+#                 ack = pack(f"!I", count)
+#                 s.sendto(ack, (HOST, PORT))
+#                 j += 1
+#         # print(f"Received {count} data units\n")
+#         # print(f"Received {len(msg) + HEADER} B packets")
+#         # print(f"SHA256 checksum of received data: {sha256(msg).hexdigest()}")
+
+
 import socket
 from struct import *
-from hashlib import sha256
 
-HOST, PORT, HEADER = "", 4950, 8
+HOST = 'localhost'  # Standard loopback interface address (localhost)
+PORT = 4950    # Port to listen on (non-privileged ports are > 1023)
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
     s.bind((HOST, PORT))
+    header, cli_addr = s.recvfrom(8)
+    file_size, data_unit_size = unpack("!II", header)
+
+    print(
+        f"Connected by {cli_addr}\n, File size for transfer from client : {file_size}\n")
+    # data_unit_size = int(s.recvfrom(4)[0].decode('utf8'))
     while True:
-            msg = bytearray()
-            count = 0
-            DATA_UNIT_SIZE, TOTAL_FILE_SIZE = unpack("!II", s.recvfrom(HEADER))
-            j = 1
-            while len(msg) < TOTAL_FILE_SIZE:
-                if(j > 3):
-                    j = 1
-                msg.extend(s.recvfrom(j*DATA_UNIT_SIZE))
-                count +=j
-                ack = pack(f"!I",count)
-                s.sendto(ack, (HOST, PORT))
-                j+=1
-        print(f"Received {len(msg) + HEADER} B packets")
-        # print(f"SHA256 checksum of received data: {sha256(msg).hexdigest()}")
+        # file_size, cli_addr = s.recvfrom(4)
+        # file_size = int(file_size.decode('utf8'))
+
+        batch = 1
+        pkt_count = 0
+        message = bytearray()
+        while len(message) < file_size:
+            if (batch > 3):
+                batch = 1
+            data = s.recvfrom(batch * data_unit_size)[0]
+            message.extend(data)
+            s.sendto(
+                bytes(str(len(message))+"\0", encoding='utf8'), cli_addr)
+            print(
+                f"Currently Received {len(message)} Bytes of data since beginning of transfer\n")
+            pkt_count += batch
+            batch += 1
